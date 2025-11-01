dbbddc1f8ed7f7e012b63d73dfbe3e3479e0f3ff
ablakanovamin0-svg <ablakanovamin0@gmail.com>
2025-11-01 23:16:59 +0300

    Создание класса списка

---
+++ src/main/java/org/example/List.java
@@ -0,0 +1,42 @@
+package org.example;
+
+import java.util.Objects;
+
+/**
+ * Класс списка для хранения
+ * произвольного числа элементов
+ * @param <T> тип данных элементов списка
+ * @author ablakanovamin0-svg
+ * @version 1.0
+ */
+public class List<T> {
+    /**
+     * Вложенный класс узла списка
+     * @param <T> тип данных элемента узла
+     */
+    private static class Node<T> {
+        T elem;
+        Node<T> nextElem;
+
+        /**
+         * Конструктор объектов класса узла списка
+         * @param data данные, которые нужно внести в узел
+         */
+        Node(T data) {
+            elem = data;
+            nextElem = null;
+        }
+    }
+    private Node<T> head;
+    private Node<T> end;
+    private int length;
+
+    /**
+     * Конструктор объектов класса списка
+     */
+    public List() {
+        head = null;
+        end = null;
+        length = 0;
+    }
+}
